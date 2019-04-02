09d39ea9c08b589924b866c1584dfcd0e6295229
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2019-04-01 17:24:22 -0700

    Added Admin LTE3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Deployed Laravel on DigitalOcean
+                    Deployed Laravel Testing AdminLTE 3
                 </div>
 
                 <div class="links">
