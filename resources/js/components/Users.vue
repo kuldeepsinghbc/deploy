85e69355db451c57dcbede34350538c17f162aea
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2019-04-02 13:28:22 -0700

    Using Axios Disaplaying Users from Database

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -14,30 +14,32 @@
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0">
                 <table class="table table-hover">
-                  <tbody><tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Type</th>
-                    <th>Modify</th>
-                  </tr>
-                  <tr>
-                    <td>183</td>
-                    <td>John Doe</td>
-                    <td>11-7-2014</td>
-                    <td><span class="tag tag-success">Approved</span></td>
-                    <td>
-                      <a href="#">
-                        <i class="fa fa-edit blue"></i>
-                      </a>/
-                      <a href="#">
-                        <i class="fa fa-trash red"></i>
-                      </a>
-                    </td>
-                  </tr>
-                  
-                  
-                </tbody></table>
+                  <tbody>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Registered At</th>
+                      <th>Modify</th>
+                    </tr>
+                    <tr v-for="user in users">
+                      <td>{{user.id}}</td>
+                      <td>{{user.name}}</td>
+                      <td>{{user.email}}</td>
+                      <td>{{user.type}}</td>
+                      <td>{{user.created_at}}</td>
+                      <td>
+                        <a href="#">
+                          <i class="fa fa-edit blue"></i>
+                        </a>/
+                        <a href="#">
+                          <i class="fa fa-trash red"></i>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
               <!-- /.card-body -->
             </div>
@@ -54,44 +56,46 @@
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div class="modal-body">
-                <div class="form-group">
-                  <input v-model="form.name" type="text" name="name"
-                      placeholder="Name"
-                      class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
-                  <has-error :form="form" field="name"></has-error>
-                  </div>
+              <form @submit.prevent="createUser">
+                <div class="modal-body">
                   <div class="form-group">
-                    <input v-model="form.email" type="email" name="email"
-                        placeholder="Email Address"
-                        class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
-                    <has-error :form="form" field="email"></has-error>
-                  </div>
-                  <div class="form-group">
-                    <textarea v-model="form.bio" name="bio" id="bio"
-                    placeholder="Short bio for user (Optional)"
-                    class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
-                    <has-error :form="form" field="bio"></has-error>
-                  </div>
-                  <div class="form-group">
-                    <select name="type" v-model="form.type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
-                        <option value="">Select User Role</option>
-                        <option value="admin">Admin</option>
-                        <option value="user">Standard User</option>
-                        <option value="author">Author</option>
-                    </select>
-                    <has-error :form="form" field="type"></has-error>
-                  </div>
-                  <div class="form-group">
-                    <input v-model="form.password" type="password" name="password" id="password"
-                    class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
-                    <has-error :form="form" field="password"></has-error>
-                  </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Create </button>
-              </div>
+                    <input v-model="form.name" type="text" name="name"
+                        placeholder="Name"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                    <has-error :form="form" field="name"></has-error>
+                    </div>
+                    <div class="form-group">
+                      <input v-model="form.email" type="email" name="email"
+                          placeholder="Email Address"
+                          class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                      <has-error :form="form" field="email"></has-error>
+                    </div>
+                    <div class="form-group">
+                      <textarea v-model="form.bio" name="bio" id="bio"
+                      placeholder="Short bio for user (Optional)"
+                      class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                      <has-error :form="form" field="bio"></has-error>
+                    </div>
+                    <div class="form-group">
+                      <select name="type" v-model="form.type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                          <option value="">Select User Role</option>
+                          <option value="admin">Admin</option>
+                          <option value="user">Standard User</option>
+                          <option value="author">Author</option>
+                      </select>
+                      <has-error :form="form" field="type"></has-error>
+                    </div>
+                    <div class="form-group">
+                      <input v-model="form.password" type="password" name="password" id="password"
+                      class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                      <has-error :form="form" field="password"></has-error>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Create </button>
+                </div>
+              </form>
             </div>
         </div>
 </div>
@@ -102,6 +106,7 @@
     export default {
         data() {
             return {
+                users : {},
                 form: new Form({
                     name : '',
                     email: '',
@@ -112,8 +117,17 @@
                 })
             }
         },
-        mounted() {
-            console.log('Component mounted.')
+        methods:{
+          loadUsers(){
+            axios.get("api/user").then(({data})=> (this.users = data.data));
+          },
+          createUser(){
+            this.form.post('api/user');
+          },
+          
+        },
+        created() {
+            this.loadUsers();
         }
     }
 </script>
